86f880f5775ba1fe029697e9ea16e392e307eaad
İbraKnk <87443921+HCan1628@users.noreply.github.com>
2021-11-02 12:10:38 +0300

    Devam

---
+++ app.js
@@ -0,0 +1,10 @@
+// JS-CC-001 : Reverse Words
+// Problem Statement
+// Write a function that takes an array of characters and reverses the words order.
+
+// Please note that, single word should not be reversed!
+
+const words = "Javascript is the best programming language"
+words.split(" ").reverse();
+
+console.log(words);
